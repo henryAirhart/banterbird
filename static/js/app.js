a2bd149190777d9c2d625dcc430dee647019bf57
henryAirhart <henry.airhart@ascte.org>
2025-04-18 10:43:13 -0500

    Added welcome message and fixed typo

--- static/js/app.js
+++ static/js/app.js
@@ -15,9 +15,10 @@ function submitPost() {
 
 window.onload = async () => {
     try {
+        alert('Welcome to Banterbird!')
         const response = await fetch("/api/posts");
         const posts = await response.json();
-        post.forEach((post) => renderPost(post));
+        posts.forEach((post) => renderPost(post));
     } catch (error) {
         console.error("Error fetching posts:", error);
     }
