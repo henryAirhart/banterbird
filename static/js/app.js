b82060aaf44152f6d18240bc5e4a25bc97f54f21
henryAirhart <henry.airhart@ascte.org>
2025-05-02 10:02:42 -0500

    add new post refresh every 5 sec

--- static/js/app.js
+++ static/js/app.js
@@ -47,3 +47,15 @@ window.onload = async () => {
         console.error("Error fetching posts:", error);
     }
 };
+
+setInterval(async() => {
+    try {
+        const response = await fetch("/api/posts");
+        const posts = await response.json();
+        posts.forEach((post) => renderPost(post));
+        document.getElementById('username').innerText = username;
+    } catch (error) {
+        console.error("Error fetching posts:", error);
+    }
+
+}, 5000);
